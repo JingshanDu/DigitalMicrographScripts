da990f07d44158188c1cfca25a4ae18e7c212cc5
Jingshan S. Du <dujingshan@live.com>
2022-12-13 01:18:07 -0800

    add new script

---
+++ Histogram_from_ROI_Manual.s
@@ -0,0 +1,31 @@
+// Create a histogram from ROI with manually defined parameters
+// Jingshan S. Du
+// jingshan.du@outlook.com
+
+image imgFront := GetFrontImage()[]
+string strImgName = GetName(imgFront)
+number numMin, numMax, numChannels
+
+// Dialog for parameter inputs
+TagGroup DLG, DLGItems
+DLG = DLGCreateDialog( "Create histogram from ROI - Manual Setup", DLGItems )
+TagGroup val1tg, val2tg, val3tg, val4tg
+
+DLGitems.DLGAddElement( DLGCreateRealField( "Min:", val1tg, -1, 8, 8 ) )        
+DLGitems.DLGAddElement( DLGCreateRealField( "Max:", val2tg, 1, 8, 8 ) )        
+DLGitems.DLGAddElement( DLGCreateRealField( "# of Channels:", val3tg, 50, 8, 8 ) )        
+
+if ( !Alloc( UIframe ).Init( DLG ).Pose() )
+ Throw( "User abort." )
+
+numMin = val1tg.DLGGetValue()
+numMax = val2tg.DLGGetValue()
+numChannels = val3tg.DLGGetValue()
+
+// Create the histogram
+image imgHist = IntegerImage("Histogram of " + strImgName, 4, 0, numChannels)
+ImageCalculateHistogram(imgFront, imgHist, numMin, numMax) 
+ImageSetDimensionOrigin(imgHist, 0, numMin)
+ImageSetDimensionScale(imgHist, 0, (numMax-numMin)/numChannels)
+
+showimage(imgHist)
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # DigitalMicrographScripts
 Some useful scripts for DigitalMicrograph
-
-## Transfer_Calibration_SI.s
-Transfer all calibration values, including x, y, and z (usually spectral axis), between Spectral Images (SI images).
+## Histogram_from_ROI_Manual.s
+It creates a histogram from the region of interest (ROI) of the front image with manually defined parameters, including lower and upper boundaries and number of binning channels.
 
 ## Math_Abs.s
 Simply calculates the absolute value of each pixel and returns the image.
+
+## Transfer_Calibration_SI.s
+Transfer all calibration values, including x, y, and z (usually spectral axis), between Spectral Images (SI images).
 
 ## More Scripts...
 I bet you already know this, but you should absolutely check out http://www.dmscripting.com/ by Dave Mitchell if you have not done so.
